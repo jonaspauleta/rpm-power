9fd99ab0716ed227c4b7afc4d2250aa84c692331
João Santos <joao.paulo@patchstack.com>
2024-07-10 01:11:58 +0100

    rpm power website

--- resources/views/components/ui/marketing/header.blade.php
+++ resources/views/components/ui/marketing/header.blade.php
@@ -1,7 +1,7 @@
 <header class="w-full">
     <div class="relative z-20 flex items-center justify-between w-full h-20 max-w-6xl px-6 mx-auto">
         <div x-data="{ mobileMenuOpen: false }" class="relative flex items-center md:space-x-2 text-neutral-800">
-            
+
             <div class="relative z-50 flex items-center w-auto h-full">
                 <a href="{{ route('home') }}" class="flex items-center mr-0 md:mr-5 shrink-0">
                     <x-ui.logo class="block w-auto text-gray-800 fill-current h-7 dark:text-gray-200" />
@@ -15,10 +15,13 @@
                 </div>
             </div>
             <div :class="{ 'flex' : mobileMenuOpen, 'hidden md:flex' :  !mobileMenuOpen }" class="fixed top-0 left-0 z-40 flex-col items-start justify-start hidden w-full h-full min-h-screen pt-20 space-y-5 text-sm font-medium duration-150 ease-out transform md:pt-0 text-neutral-500 md:h-auto md:min-h-0 md:left-auto md:items-center md:relative">
-                
+
                 <nav class="flex flex-col w-full p-6 space-y-2 bg-white md:p-0 md:flex-row md:space-x-2 md:space-y-0 md:w-auto md:bg-transparent md:flex">
                     <x-ui.nav-link href="/">Home</x-ui.nav-link>
                     <x-ui.nav-link href="/genesis/about">About</x-ui.nav-link>
+                    <x-ui.nav-link href="/sobre">Sobre</x-ui.nav-link>
+                    <x-ui.nav-link href="/produtos">Produtos</x-ui.nav-link>
+                    <x-ui.nav-link href="/contactos">Contactos</x-ui.nav-link>
                     @if(view()->exists('pages.blog.index'))
                         <x-ui.nav-link href="/blog">Blog</x-ui.nav-link>
                     @endif
@@ -42,7 +45,7 @@
                     <x-ui.button type="primary" submit="true" tag="a" href="{{ route('register') }}">Sign Up</x-ui.button>
                 </div>
             @endauth
-            
+
         </div>
     </div>
 </header>
